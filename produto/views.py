5ff06c96d914e1d311e553be9401f778bf271ca0
Anderson <gaabrieelbarbosaa@gmail.com>
2024-04-01 16:01:56 -0300

    implementação de datalhes ddo produto e variaçoes

--- produto/views.py
+++ produto/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.views import View
 from . import models
 
@@ -9,8 +10,11 @@ class ListaProdutos(ListView):
      context_object_name = 'produtos'
      paginate_by = 3
 
-class DetalheProduto(View):
-     pass
+class DetalheProduto(DetailView):
+    model = models.Produto
+    template_name = 'produto/detalhe.html'
+    context_object_name = 'produto'
+    slug_url_kwarg = 'slug'
 
 
 class AdicionarAoCarrinho(View):
